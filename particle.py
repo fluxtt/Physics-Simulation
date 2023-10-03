a52ecaed8b2e6b1eb1d9bbeccbd65a410d1fa341
James Tibbott <100445187+fluxtt@users.noreply.github.com>
2023-10-03 03:07:09 +0100

    add pymunk setup

--- particle.py
+++ particle.py
@@ -4,6 +4,9 @@ pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
 
+space = pymunk.Space() # create physical space
+space.gravity = (0,500) # x and y gravity
+
 while True: # Game loop
     for event in pygame.event.get(): # checking for user input
         if event.type == pygame.QUIT: # input to close the game
@@ -11,6 +14,7 @@ while True: # Game loop
             sys.exit()
             
     screen.fill((217,217,217)) # background colour
+    space.step(1/50)
     pygame.display.update() # rendering the frame
     clock.tick(120) # limit the frames per second to 120        
     
